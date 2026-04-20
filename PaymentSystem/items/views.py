ef3ea0f7625abfc70e0a7bf73a9c879f285a2013
Furasfast346 <zahar.zaytsev.2007@gmail.com>
2026-04-20 19:37:21 +0300

    Подключил stripe, сделал редирект на платежку, вынес секретные ключи в переменные окружения

--- PaymentSystem/items/views.py
+++ PaymentSystem/items/views.py
@@ -1,10 +1,31 @@
+from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404
 from .models import Item
+import stripe
+from django.conf import settings
 
 def item_view(request, pk):
     item = get_object_or_404(Item, pk=pk)
     context = {'item' : item}
     return render(request, 'index.html', context)
 
-def buy_view(request):
-    pass
+def buy_view(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+    session = stripe.checkout.Session.create(
+        line_items=[{
+            'price_data': {
+                'currency': 'rub',
+                'unit_amount': int(item.price * 100),  # в копейки!
+                'product_data': {
+                    'name': item.name,
+                    'description': item.description,
+                },
+            },
+            'quantity': 1,
+        }],
+        mode='payment',
+        success_url='http://127.0.0.1:8000/item/' + str(pk),
+        cancel_url='http://127.0.0.1:8000/item/'  + str(pk),
+    )
+    return JsonResponse({'session_id' : session.id})
