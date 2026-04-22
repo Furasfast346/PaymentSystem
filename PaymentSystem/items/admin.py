3ec5e5303ed2ffafeeb1bcd9caf1f5191089f25b
Furasfast346 <zahar.zaytsev.2007@gmail.com>
2026-04-22 20:06:31 +0300

    Добавил модели Order, Tax, Discount, написал код для оплаты заказа

--- PaymentSystem/items/admin.py
+++ PaymentSystem/items/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
-from .models import Item
+from .models import Item, Order, Tax, Discount, OrderItem
 
-@admin.register(Item)
-class ItemAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', 'price')
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 1
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    inlines = [OrderItemInline]
+
+admin.site.register(Item)
+admin.site.register(Tax)
+admin.site.register(Discount)
--- PaymentSystem/items/models.py
+++ PaymentSystem/items/models.py
@@ -4,3 +4,36 @@ class Item(models.Model):
     name = models.CharField(max_length=150)
     description = models.TextField(max_length=1000, blank=True)
     price = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+class Discount(models.Model):
+    name = models.CharField(max_length=150)
+    percentage = models.IntegerField()
+    stripe_id = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Tax(models.Model):
+    name = models.CharField(max_length=150)
+    percentage = models.IntegerField()
+    stripe_id = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return self.name
+
+class Order(models.Model):
+    item = models.ManyToManyField(Item, through='OrderItem')
+    tax = models.ForeignKey(Tax, on_delete=models.SET_NULL, blank=True, null=True)
+    discount = models.ForeignKey(Discount, on_delete=models.SET_NULL, blank=True, null=True)
+
+    def __str__(self):
+        return "Order " + str(self.id)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
